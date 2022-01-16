5caae9105a2fc55ed76f7ecd3769f6df3de5ce47
Francisco Sánchez <sanchezgarridof@gmail.com>
2022-01-17 00:21:04 +0100

    List Todos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([
+    "Hacer proyecto React Firebase",
+    "Grabar proyecto React",
+  ]);
+
+  const [input, setInput] = useState("");
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    setTodos([...todos, input]);
+    setInput("");
+  };
+
   return (
     <div className="App">
       <h1>TODO React Firebase</h1>
+      <form>
+        <input value={input} onChange={(e) => setInput(e.target.value)} />
+        <button type="submit" onClick={addTodo}>
+          Add Todo
+        </button>
+      </form>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo}>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
