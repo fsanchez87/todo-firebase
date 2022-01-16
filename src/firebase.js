9937ec6a950f7d261b1e830c00f4126eca868a1a
Francisco Sánchez <sanchezgarridof@gmail.com>
2022-01-17 00:02:41 +0100

    Basic React Setup

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCwEl6-BwFlmYBaUlqsegfHsu7Vo3bZK5g",
+  authDomain: "todo-react-firebase-e754d.firebaseapp.com",
+  projectId: "todo-react-firebase-e754d",
+  storageBucket: "todo-react-firebase-e754d.appspot.com",
+  messagingSenderId: "925550223517",
+  appId: "1:925550223517:web:666d7ad7d4b4a8df53b474",
+  measurementId: "G-CSQS632007",
+};
